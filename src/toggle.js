2750819cdff78af86f43c8c18bf3bbf0d5d9ac52
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2023-06-05 00:57:22 +0900

    Set favorite theme to localStorage

--- src/toggle.js
+++ src/toggle.js
@@ -1,5 +1,25 @@
 const themeToggler = document.querySelector('#themeToggler');
 
+const KEY = 'favTheme';
+
+const favTheme = localStorage.getItem(KEY);
+
+if (favTheme !== null) {
+  const toggler = document.querySelector('#themeToggler');
+  document.body.classList.add(favTheme);
+  switch (favTheme) {
+    case 'theme1':
+      toggler.value = 0;
+      break;
+    case 'theme2':
+      toggler.value = 1;
+      break;
+    case 'theme3':
+      toggler.value = 2;
+      break;
+  }
+}
+
 // Toggle color themes
 themeToggler.addEventListener('change', (event) => {
   const themeNumber = event.target.value;
@@ -12,6 +32,7 @@ themeToggler.addEventListener('change', (event) => {
         document.body.classList.remove('theme3');
       }
       document.body.classList.add('theme1');
+      localStorage.setItem(KEY, 'theme1');
       break;
     case '1':
       if (document.body.classList.contains('theme3')) {
@@ -21,6 +42,7 @@ themeToggler.addEventListener('change', (event) => {
         document.body.classList.remove('theme1');
       }
       document.body.classList.add('theme2');
+      localStorage.setItem(KEY, 'theme2');
       break;
     default:
       if (document.body.classList.contains('theme1')) {
@@ -30,5 +52,6 @@ themeToggler.addEventListener('change', (event) => {
         document.body.classList.remove('theme2');
       }
       document.body.classList.add('theme3');
+      localStorage.setItem(KEY, 'theme3');
   }
 });
