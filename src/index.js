7a89f18d9f764f03349641cfc1f4c3713a403b7c
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2023-06-05 21:06:04 +0900

    Turn index.js and toggle.js into iife style

--- src/index.js
+++ src/index.js
@@ -1,142 +1,144 @@
-const BUTTON = '[object HTMLButtonElement]';
-const keypadContainer = document.querySelector('#keypad-container');
-const result = document.querySelector('#result');
-result.innerText = 0;
+(() => {
+  const BUTTON = '[object HTMLButtonElement]';
+  const keypadContainer = document.querySelector('#keypad-container');
+  const result = document.querySelector('#result');
+  result.innerText = 0;
 
-let state = '';
+  let state = '';
 
-const one = document.querySelector('#one');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
-const seven = document.querySelector('#seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
-const zero = document.querySelector('#zero');
-const dot = document.querySelector('#dot');
+  const one = document.querySelector('#one');
+  const two = document.querySelector('#two');
+  const three = document.querySelector('#three');
+  const four = document.querySelector('#four');
+  const five = document.querySelector('#five');
+  const six = document.querySelector('#six');
+  const seven = document.querySelector('#seven');
+  const eight = document.querySelector('#eight');
+  const nine = document.querySelector('#nine');
+  const zero = document.querySelector('#zero');
+  const dot = document.querySelector('#dot');
 
-const add = document.querySelector('#add');
-const div = document.querySelector('#div');
-const mul = document.querySelector('#mul');
-const sub = document.querySelector('#sub');
+  const add = document.querySelector('#add');
+  const div = document.querySelector('#div');
+  const mul = document.querySelector('#mul');
+  const sub = document.querySelector('#sub');
 
-const ops = [add, div, mul, sub];
+  const ops = [add, div, mul, sub];
 
-ops.forEach((op) => {
-  op.addEventListener('click', (event) => {
+  ops.forEach((op) => {
+    op.addEventListener('click', (event) => {
+      if (state.length === 0) {
+        return;
+      }
+      if (isOp(state[state.length - 1])) {
+        state = state.slice(0, state.length - 1) + event.currentTarget.value;
+        result.innerText = state;
+        return;
+      }
+      if (state.at(-1) === '.') {
+        return;
+      }
+      state += event.target.value;
+      result.innerText = state;
+    });
+  });
+
+  const nums = [one, two, three, four, five, six, seven, eight, nine];
+  nums.forEach((n, i) => {
+    n.addEventListener('click', (event) => {
+      if (state.at(-1) === '0' && state.length === 1) {
+        state = `${i + 1}`;
+        result.innerText = state;
+        return;
+      }
+      state += `${i + 1}`;
+      result.innerText = state;
+    });
+  });
+
+  zero.addEventListener('click', (event) => {
+    // if (state.length === 0) {
+    //   return;
+    // }
+    let i = state.length;
+    let n = '';
+    while (i > 0 && !isOp(state[i])) {
+      n += state[i - 1];
+      i--;
+    }
+    if (isOp(n.at(-1))) {
+      n = n.slice(0, n.length - 1);
+    }
+    if (n === '0') {
+      return;
+    }
+    state += event.currentTarget.value.toString();
+    result.innerText = state;
+  });
+
+  const enter = document.querySelector('#enter');
+  const reset = document.querySelector('#reset');
+  const clear = document.querySelector('#clear');
+
+  enter.addEventListener('click', (event) => {
     if (state.length === 0) {
       return;
     }
-    if (isOp(state[state.length - 1])) {
-      state = state.slice(0, state.length - 1) + event.currentTarget.value;
-      result.innerText = state;
+    const op = state.at(-1);
+    const v = Number.parseFloat(state.slice(0, state.length - 1));
+    if (
+      !isNaN(v) &&
+      (Number.isSafeInteger(v) || Number.isFinite(v)) &&
+      isOp(op)
+    ) {
+      state += `${v}`;
+    }
+    const lexer = new Lexer(state);
+    const parser = new Parser(lexer);
+    const calculator = new Calculator(parser);
+    const n = calculator.cc();
+    let strNum = n.toString();
+    if (strNum.includes('.')) {
+      const dotIndex = strNum.indexOf('.');
+      let decimalPointValue = strNum.slice(dotIndex + 1);
+      if (decimalPointValue.length > 2) {
+        decimalPointValue = decimalPointValue.slice(0, 2);
+      }
+      const integer = strNum.slice(0, dotIndex);
+      strNum = `${integer}.${decimalPointValue}`;
+    }
+    result.innerText = n === Infinity || isNaN(n) ? 0 : strNum;
+    state = n === Infinity || isNaN(n) ? '0' : strNum;
+  });
+
+  reset.addEventListener('click', (event) => {
+    state = '';
+    result.innerText = 0;
+  });
+
+  clear.addEventListener('click', (event) => {
+    state = state.slice(0, state.length - 1);
+    result.innerText = state.length === 0 ? 0 : state;
+  });
+
+  dot.addEventListener('click', (event) => {
+    // When nothing entered
+    if (state.length === 0) {
       return;
     }
-    if (state.at(-1) === '.') {
+    if (isOp(state.at(-1))) {
       return;
     }
-    state += event.target.value;
+
+    let i = state.length;
+    while (i >= 0 && !isOp(state[i])) {
+      if (state[i] === '.') {
+        return;
+      }
+      i--;
+    }
+
+    state += '.';
     result.innerText = state;
   });
-});
-
-const nums = [one, two, three, four, five, six, seven, eight, nine];
-nums.forEach((n, i) => {
-  n.addEventListener('click', (event) => {
-    if (state.at(-1) === '0' && state.length === 1) {
-      state = `${i + 1}`;
-      result.innerText = state;
-      return;
-    }
-    state += `${i + 1}`;
-    result.innerText = state;
-  });
-});
-
-zero.addEventListener('click', (event) => {
-  // if (state.length === 0) {
-  //   return;
-  // }
-  let i = state.length;
-  let n = '';
-  while (i > 0 && !isOp(state[i])) {
-    n += state[i - 1];
-    i--;
-  }
-  if (isOp(n.at(-1))) {
-    n = n.slice(0, n.length - 1);
-  }
-  if (n === '0') {
-    return;
-  }
-  state += event.currentTarget.value.toString();
-  result.innerText = state;
-});
-
-const enter = document.querySelector('#enter');
-const reset = document.querySelector('#reset');
-const clear = document.querySelector('#clear');
-
-enter.addEventListener('click', (event) => {
-  if (state.length === 0) {
-    return;
-  }
-  const op = state.at(-1);
-  const v = Number.parseFloat(state.slice(0, state.length - 1));
-  if (
-    !isNaN(v) &&
-    (Number.isSafeInteger(v) || Number.isFinite(v)) &&
-    isOp(op)
-  ) {
-    state += `${v}`;
-  }
-  const lexer = new Lexer(state);
-  const parser = new Parser(lexer);
-  const calculator = new Calculator(parser);
-  const n = calculator.cc();
-  let strNum = n.toString();
-  if (strNum.includes('.')) {
-    const dotIndex = strNum.indexOf('.');
-    let decimalPointValue = strNum.slice(dotIndex + 1);
-    if (decimalPointValue.length > 3) {
-      decimalPointValue = decimalPointValue.slice(0, 3);
-    }
-    const integer = strNum.slice(0, dotIndex);
-    strNum = `${integer}.${decimalPointValue}`;
-  }
-  result.innerText = n === Infinity || isNaN(n) ? 0 : strNum;
-  state = n === Infinity || isNaN(n) ? '0' : strNum;
-});
-
-reset.addEventListener('click', (event) => {
-  state = '';
-  result.innerText = 0;
-});
-
-clear.addEventListener('click', (event) => {
-  state = state.slice(0, state.length - 1);
-  result.innerText = state.length === 0 ? 0 : state;
-});
-
-dot.addEventListener('click', (event) => {
-  // When nothing entered
-  if (state.length === 0) {
-    return;
-  }
-  if (isOp(state.at(-1))) {
-    return;
-  }
-
-  let i = state.length;
-  while (i >= 0 && !isOp(state[i])) {
-    if (state[i] === '.') {
-      return;
-    }
-    i--;
-  }
-
-  state += '.';
-  result.innerText = state;
-});
+})();
--- src/toggle.js
+++ src/toggle.js
@@ -1,57 +1,59 @@
-const themeToggler = document.querySelector('#themeToggler');
+(() => {
+  const themeToggler = document.querySelector('#themeToggler');
 
-const KEY = 'favTheme';
+  const KEY = 'favTheme';
 
-const favTheme = localStorage.getItem(KEY);
+  const favTheme = localStorage.getItem(KEY);
 
-if (favTheme !== null) {
-  const toggler = document.querySelector('#themeToggler');
-  document.body.classList.add(favTheme);
-  switch (favTheme) {
-    case 'theme1':
-      toggler.value = 0;
-      break;
-    case 'theme2':
-      toggler.value = 1;
-      break;
-    case 'theme3':
-      toggler.value = 2;
-      break;
+  if (favTheme !== null) {
+    const toggler = document.querySelector('#themeToggler');
+    document.body.classList.add(favTheme);
+    switch (favTheme) {
+      case 'theme1':
+        toggler.value = 0;
+        break;
+      case 'theme2':
+        toggler.value = 1;
+        break;
+      case 'theme3':
+        toggler.value = 2;
+        break;
+    }
   }
-}
 
-// Toggle color themes
-themeToggler.addEventListener('change', (event) => {
-  const themeNumber = event.target.value;
-  switch (themeNumber) {
-    case '0':
-      if (document.body.classList.contains('theme2')) {
-        document.body.classList.remove('theme2');
-      }
-      if (document.body.classList.contains('theme3')) {
-        document.body.classList.remove('theme3');
-      }
-      document.body.classList.add('theme1');
-      localStorage.setItem(KEY, 'theme1');
-      break;
-    case '1':
-      if (document.body.classList.contains('theme3')) {
-        document.body.classList.remove('theme3');
-      }
-      if (document.body.classList.contains('theme1')) {
-        document.body.classList.remove('theme1');
-      }
-      document.body.classList.add('theme2');
-      localStorage.setItem(KEY, 'theme2');
-      break;
-    default:
-      if (document.body.classList.contains('theme1')) {
-        document.body.classList.remove('theme1');
-      }
-      if (document.body.classList.contains('theme2')) {
-        document.body.classList.remove('theme2');
-      }
-      document.body.classList.add('theme3');
-      localStorage.setItem(KEY, 'theme3');
-  }
-});
+  // Toggle color themes
+  themeToggler.addEventListener('change', (event) => {
+    const themeNumber = event.target.value;
+    switch (themeNumber) {
+      case '0':
+        if (document.body.classList.contains('theme2')) {
+          document.body.classList.remove('theme2');
+        }
+        if (document.body.classList.contains('theme3')) {
+          document.body.classList.remove('theme3');
+        }
+        document.body.classList.add('theme1');
+        localStorage.setItem(KEY, 'theme1');
+        break;
+      case '1':
+        if (document.body.classList.contains('theme3')) {
+          document.body.classList.remove('theme3');
+        }
+        if (document.body.classList.contains('theme1')) {
+          document.body.classList.remove('theme1');
+        }
+        document.body.classList.add('theme2');
+        localStorage.setItem(KEY, 'theme2');
+        break;
+      default:
+        if (document.body.classList.contains('theme1')) {
+          document.body.classList.remove('theme1');
+        }
+        if (document.body.classList.contains('theme2')) {
+          document.body.classList.remove('theme2');
+        }
+        document.body.classList.add('theme3');
+        localStorage.setItem(KEY, 'theme3');
+    }
+  });
+})();
